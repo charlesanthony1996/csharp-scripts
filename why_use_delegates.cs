7b7d0e37cffc16c95fc8cca6d0d96b442d33e913
charlesanthony1996 <charlesanthony296@gmail.com>
2025-06-22 15:21:48 +0200

    some comments

--- why_use_delegates.cs
+++ why_use_delegates.cs
@@ -1,23 +1,23 @@
-# a delegate is a type safe function pointer. it holds a reference to a method
+// # a delegate is a type safe function pointer. it holds a reference to a method
 
-# meaning it can point to any method with a matching signature
+// # meaning it can point to any method with a matching signature
 
-# why use delegates
+// # why use delegates
 
-# 1. The allow you to pass methods as parameters
+// # 1. The allow you to pass methods as parameters
 
-# 2. They enable callbacks and event handling
+// # 2. They enable callbacks and event handling
 
-# 3. They support flexible, dynamic method invocation
+// # 3. They support flexible, dynamic method invocation
 
 
-# more tips on what delegates are
+// # more tips on what delegates are
 
-# -> a delegate is like a blueprint describing a method signature -- which parameters and return type a method must have
+// # -> a delegate is like a blueprint describing a method signature -- which parameters and return type a method must have
 
-# -> when you create a delegate instance, you assign it a method that matches that signature
+// # -> when you create a delegate instance, you assign it a method that matches that signature
 
-# -> then you can call the method via the delegate instance
+// # -> then you can call the method via the delegate instance
 
 using System;
 
